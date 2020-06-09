2c9a5681f4f31620720df79ff8221658ddc3d107
David Hutchison <dhutchis68516@gmail.com>
2020-06-09 14:46:23 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,6 +20,10 @@ class InteractiveRecord
   end
 
   def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
     self.class.column_names.delete_if{|column| column == "id"}.join(", ")
   end
 
