192f4a59e85c3e0ddbb8da113052e72a1f22300c
David Hutchison <dhutchis68516@gmail.com>
2020-06-09 14:30:07 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,9 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def initialize (h_arr = {})
+    h_arr.each do | key, value |
+      self.send("#{key}=",value)
+    end 
+  end 
 end
